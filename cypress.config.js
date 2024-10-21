2618226f5a433ba60dd5ca33f8c8eae1d0f83b16
avsbutt <awais.3utt@gmail.com>
2024-10-21 11:05:26 +0500

    OLA Project setup

---
+++ cypress.config.js
@@ -0,0 +1,39 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+
+
+    specPattern: "cypress/e2e/TestCases/**/*.cy.js",     //The specPattern tells Cypress where to find your test files.
+
+   // baseUrl: "",
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    
+    fixturesFolder: 'cypress/e2e/fixtures',
+    tsc: 'cypress/support/commands.ts',
+    defaultCommandTimeout: 10000,
+
+   
+    video: false,
+    videosFolder: 'cypress/e2e/videos',
+    screenshotsFolder: 'cypress/e2e/screenshots',
+    pageLoadTimeout:60000 ,
+    screenshots: false,
+    
+    viewportWidth: 1280,
+    viewportHeight: 800,
+  
+    
+
+    screenshotOnRunFailure: true,
+    reporter: 'mocha-allure-reporter',
+    reporterOptions: {
+      targetDir: 'allure-results',
+      "viewportWidth":1280
+
+    }
+  },
+});
