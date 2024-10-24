a94ab33b022df3dabc4e77f36749bc848af347d5
avsbutt <awais.3utt@gmail.com>
2024-10-24 09:32:21 +0500

    Employment Info Inprogress

---
+++ cypress/e2e/Pages/Client/createNewAccount/Personal/Individual/EmploymentInfomationPage.js
@@ -0,0 +1,12 @@
+import { EmployedLocators } from "../../../../../Locators/Personal/EmploymentInformationLocators.json"
+import { RetiredLocators } from "../../../../../Locators/Personal/EmploymentInformationLocators.json"
+import { UnEmployedLocators } from "../../../../../Locators/Personal/EmploymentInformationLocators.json"
+import { FormUsageButtons } from "../../../../../Locators/FormUsageButtons.json";
+
+export class EmploymentInformationPage{
+
+ClickOnUnemployed(){
+    cy.xpath(UnEmployedLocators)
+}
+    
+}
