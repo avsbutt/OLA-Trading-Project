3ee13683cadb133b47708ccdc45971236b04bead
avsbutt <awais.3utt@gmail.com>
2024-12-20 11:33:53 +0500

    #Cypress Updates #RR Login Utils Added

--- cypress/e2e/Utils/LoginUtils.js
+++ cypress/e2e/Utils/LoginUtils.js
@@ -14,14 +14,25 @@ export function clientLoginUtils() {
   })
 
   cy.fixture('UsersCredential.json').then((data) => {
-   cy.xpath(loginLocators.username).type(data.clientCrendetial.username);  
-   cy.xpath(loginLocators.password).type(data.clientCrendetial.password); 
-  });
+   cy.xpath(loginLocators.username).type(data.clientCrendetial.username)  
+   cy.xpath(loginLocators.password).type(data.clientCrendetial.password) 
+  })
 
   cy.xpath(loginLocators.loginBtn).click()
   cy.url().should('include', '/dashboard')
 }
 
-export function brokerLoginUtils(){
+export function RegisterRepresentativeLoginUtils(){
+  cy.visit("", {failOnStatusCode: false, timeout: 20000,
+    auth: {
+      username: 'ola-staging',
+      password: 'Atlasclear@123/'
+     }
+  })
+ 
+   cy.fixture('UsersCredential.json').then((data) => {
+      cy.xpath(loginLocators.username).type(data.rrCredential.username)  
+      cy.xpath(loginLocators.password).type(data.rrCredential.password)
+   })
   
 }
